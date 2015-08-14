50ee06a78d42b9442e21d5bf7064d473b8019336
yangrui@foresee.cn <yangrui@foresee.cn@7450c7bb-9219-402f-bd4d-d5999c0c4929>
2015-08-14 15:17:48 +0000

    更新xml导入和导出功能
    git-svn-id: https://svn.foresee.com.cn/svn/new_omni/trunk/engineering/src/delphi/src/tools/DatManage@63742 7450c7bb-9219-402f-bd4d-d5999c0c4929

--- FileHandle/unitXmlHandle.pas
+++ FileHandle/unitXmlHandle.pas
@@ -10,13 +10,19 @@ type
   TXmlHandle = class(TFileHandle)
   private
     FTable : TTable;
-    XMLDocument1: TXMLDocument;    
+    XMLFile: TXMLDocument;
+    XMLRead : TXMLDocument;
+    FAppPath : String;
+    FSavePath : String;
+    FConfigFile : String;
+    procedure SetPath;
   protected
     procedure LoadFile; override;
   public
     destructor Destroy; override;
     constructor Create(aTable : TTable);
     procedure SaveFile;
+    procedure ReadFile;
   end;
 
 
@@ -26,10 +32,17 @@ implementation
 constructor TXmlHandle.Create(aTable : TTable);
 begin
   FTable := aTable;
-  XMLDocument1 := TXMLDocument.Create(nil);   
+  XMLFile := TXMLDocument.Create(nil);
+  SetPath;
   LoadFile;
 end;
 
+procedure TXmlHandle.SetPath;
+begin
+  FAppPath := ExtractFileDir(ParamStr(0));
+  FSavePath := FAppPath + '\Template';
+  FConfigFile := FSavePath + '\'+ FTable.TableName + '.xml';
+end;
 
 
 procedure TXmlHandle.LoadFile;
@@ -37,25 +50,58 @@ begin
   inherited;
 end;
 
+procedure TXmlHandle.ReadFile;
+var
+  nodeList: IXMLNodeList;
+  node: IXMLNode;
+  num,i: Integer;
+  comp : TComponent;
+begin
+  if not FileExists(FConfigFile) then
+  begin
+    Exit;
+  end;
+  comp :=TComponent.create(nil);
+  XMLRead := TXMLDocument.Create(comp);
+  XMLRead.LoadFromFile(FConfigFile);
+  
+  for I:=0 to FTable.TableFieldCount - 1 do
+  begin
+     FTable.TableFieldCaptionArray[I] :=  XMLRead.DocumentElement.ChildNodes[I].ChildNodes['Caption'].Text;
+     FTable.TableFieldSizeArray[I] :=  StrToInt(XMLRead.DocumentElement.ChildNodes[I].ChildNodes['Size'].Text);
+  end;
+end;
+
 
 procedure TXmlHandle.SaveFile;
 var
-  pNode,cNode: IXMLNode; {定义两个节点: 父节点、子节点}
+  pNode,tNode,cNode: IXMLNode; {定义两个节点: 父节点、子节点}
+  I : Integer;
 begin
-  XMLDocument1.XML.Clear;
-  XMLDocument1.Active := True;                {必须先激活}
-  XMLDocument1.Version := '1.0';              {设置版本}
-  XMLDocument1.Encoding := 'GB2312';          {设置语言}
+  XMLFile.XML.Clear;
+  XMLFile.Active := True;                {必须先激活}
+  XMLFile.Version := '1.0';              {设置版本}
+  XMLFile.Encoding := 'GB2312';          {设置语言}
 
-  pNode := XMLDocument1.AddChild('科室名单'); {添加的第一个节点是根节点, 现在的 pNode 是根节点}
-  pNode.SetAttribute('备注', '测试');         {为根节点设置属性}
+  pNode := XMLFile.AddChild('TableConfig'); {添加的第一个节点是根节点, 现在的 pNode 是根节点}
+  pNode.SetAttribute('TableName',FTable.TableName);         {为根节点设置属性}
+  pNode.SetAttribute('TableFieldCount',FTable.TableFieldCount);         {为根节点设置属性}
 
-  pNode := pNode.AddChild('人员');            {为根节点添加子节点, 现在的 pNode 是 "人员" 节点}
-  pNode.SetAttribute('职务', '科长');         {设置属性}
-  pNode.SetAttribute('备注', '正局级');
 
-//  cNode := pNode.AddChild('姓名');  {为 pNode 添加子节点, 返回值 cNode 指向了新添加的节点}
-//  cNode.Text := '张三';
+
+  for I:=0 to FTable.TableFieldCount - 1 do
+  begin
+    tNode := pNode.AddChild(FTable.TableFieldNameArray[I]);            {为根节点添加子节点, 现在的 pNode 是 "人员" 节点}
+//    tNode.SetAttribute('职务', '科长');         {设置属性}
+
+    cNode := tNode.AddChild('Caption');  {为 pNode 添加子节点, 返回值 cNode 指向了新添加的节点}
+    cNode.Text := FTable.TableFieldCaptionArray[I];
+
+    cNode := tNode.AddChild('Size');  {为 pNode 添加子节点, 返回值 cNode 指向了新添加的节点}
+    cNode.Text := IntToStr(FTable.TableFieldSizeArray[I]);
+  end;
+
+
 //
 //  cNode := pNode.AddChild('性别');
 //  cNode.Text := '男';
@@ -63,11 +109,23 @@ begin
 //  cNode := pNode.AddChild('年龄');
 //  cNode.Text := '34';
 
-  {查看}
-//  ShowMessage(XMLDocument1.XML.Text);
+//  {查看}
+//  ShowMessage(XMLFile.XML.Text);
+
+  if not DirectoryExists(FSavePath) then
+  try
+    begin
+      CreateDir(FSavePath);
+//      ForceDirectories(Edit1.Text);
+    end;
+  finally
+    raise Exception.Create('Cannot Create '+FSavePath);
+  end;
+
 
   {保存}
-  XMLDocument1.SaveToFile('E:\临时使用\2.xml');
+  XMLFile.SaveToFile(FConfigFile);
+  ShowMessage('生成'+FConfigFile+ '成功');
 end;
 
 
