557f42392d0bf85edbc5a9facebf51d53222bc8b
yangrui@foresee.cn <yangrui@foresee.cn@7450c7bb-9219-402f-bd4d-d5999c0c4929>
2015-09-14 15:47:27 +0000

    增加关于
    git-svn-id: https://svn.foresee.com.cn/svn/new_omni/trunk/engineering/src/delphi/src/tools/DatManage@64929 7450c7bb-9219-402f-bd4d-d5999c0c4929

---
+++ formAbout.pas
@@ -0,0 +1,52 @@
+unit formAbout;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, dxLayoutControl, cxContainer, cxEdit, cxImage, cxControls,
+  ExtCtrls, Menus, cxLookAndFeelPainters, StdCtrls, cxButtons, cxLabel,
+  RzStatus,formParent;
+
+type
+  TfmAbout = class(TParentForm)
+    pnlAbout: TPanel;
+    lcMainGroup_Root: TdxLayoutGroup;
+    lcMain: TdxLayoutControl;
+    ImgIcon: TcxImage;
+    lcMainItem1: TdxLayoutItem;
+    lblCaption: TcxLabel;
+    lcMainItem2: TdxLayoutItem;
+    btnOK: TcxButton;
+    lcMainItem3: TdxLayoutItem;
+    lblVersion: TcxLabel;
+    lcMainItem4: TdxLayoutItem;
+    lcMainGroup3: TdxLayoutGroup;
+    RzVersionInfo: TRzVersionInfo;
+    procedure btnOKClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fmAbout: TfmAbout;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfmAbout.btnOKClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfmAbout.FormShow(Sender: TObject);
+begin
+  RzVersionInfo.FilePath := Application.Exename;
+  lblVersion.EditValue := RzVersionInfo.FileVersion;
+end;
+
+end.
