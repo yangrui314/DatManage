e00265a2b66cff8fa82459a52ff81612d3240e2c
yangrui@foresee.cn <yangrui@foresee.cn@7450c7bb-9219-402f-bd4d-d5999c0c4929>
2015-09-15 06:01:24 +0000

    保存路径的功能。
    git-svn-id: https://svn.foresee.com.cn/svn/new_omni/trunk/engineering/src/delphi/src/tools/DatManage@64950 7450c7bb-9219-402f-bd4d-d5999c0c4929

---
+++ formSavePath.pas
@@ -0,0 +1,68 @@
+unit formSavePath;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, formParent, dxLayoutControl, cxControls, cxContainer, cxEdit,
+  cxTextEdit, Menus, cxLookAndFeelPainters, StdCtrls, cxButtons;
+
+type
+  TfmSavePath = class(TParentForm)
+    dxLayoutControl1Group_Root: TdxLayoutGroup;
+    dxLayoutControl1: TdxLayoutControl;
+    edtPath: TcxTextEdit;
+    dxLayoutControl1Item1: TdxLayoutItem;
+    dxLayoutControl1Item2: TdxLayoutItem;
+    edtName: TcxTextEdit;
+    btnCancel: TcxButton;
+    dxLayoutControl1Item3: TdxLayoutItem;
+    dxLayoutControl1Item4: TdxLayoutItem;
+    btnOK: TcxButton;
+    dxLayoutControl1Group1: TdxLayoutGroup;
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnOKClick(Sender: TObject);
+  private
+    FName : String;
+    FPath : String;
+  public
+    constructor Create(AOwner: TComponent;aName: String;aPath: String );
+    property PathName: String read FName write FName;
+    property Path: String read FPath write FPath;
+  end;
+
+var
+  fmSavePath: TfmSavePath;
+
+implementation
+
+  uses
+    unitConfig;
+
+
+{$R *.dfm}
+
+
+constructor TfmSavePath.Create(AOwner: TComponent; aName: String ; aPath: String );
+begin
+  inherited Create(AOwner);
+  FName := aName;
+  FPath := aPath;
+  edtName.EditValue := FName;
+  edtPath.EditValue := FPath;
+end;
+
+procedure TfmSavePath.btnCancelClick(Sender: TObject);
+begin
+  inherited;
+  Close;
+end;
+
+procedure TfmSavePath.btnOKClick(Sender: TObject);
+begin
+  inherited;
+  FName := edtName.EditValue;
+  FPath := edtPath.EditValue;
+end;
+
+end.
