22e615f56bfb6dc28a0ced54cb279aae3ea3d0cd
yangrui@foresee.cn <yangrui@foresee.cn@7450c7bb-9219-402f-bd4d-d5999c0c4929>
2015-11-04 07:11:28 +0000

    修正界面的皮肤。引用不能缺失dxSkinsForm。
    git-svn-id: https://svn.foresee.com.cn/svn/new_omni/trunk/engineering/src/delphi/src/tools/DatManage@66283 7450c7bb-9219-402f-bd4d-d5999c0c4929

--- Parent/formParent.pas
+++ Parent/formParent.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, cxLookAndFeels;
+  Dialogs,dxSkinsForm, cxLookAndFeels;
 
 type
   TParentForm = class(TForm)
