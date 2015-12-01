08564bdb1a0fb5e90016679a63f664ebe3c54c16
yr <yangrui314@126.com>
2015-12-01 23:40:59 +0800

    菜单独立一个单元

--- DatManage.pas
+++ DatManage.pas
@@ -13,7 +13,7 @@ uses
   Menus, cxLookAndFeelPainters, cxButtons, cxGridExportLink, unitConfigFile,
   unitConfigDat, formParent, cxPC, ShellAPI, WinSkinData, dxBar,formSVN,
   cxLookAndFeels, RzStatus,formUpgradeProgress,unitDownLoadFile, cxLabel,
-  unitSQLEnvironment,unitDbisamEnvironment,formParentMenu;
+  unitSQLEnvironment,unitDbisamEnvironment,formParentMenu,unitLoadMenu;
 
 type
   TfmMain = class(TParentForm)
@@ -97,7 +97,6 @@ type
     FNowVersion : string;
     FInitConnectWay : String;
 
-    procedure MenuClick(Sender: TObject);
     procedure LoadTableName(aFilter : String = '');
     procedure AddTable(const aTableName: string);
     procedure LoadField(aSQL: string);
@@ -109,7 +108,6 @@ type
     procedure ShowResult(bShow: Boolean); overload;
     procedure ShowResult; overload;
     procedure LoadConfig;
-    procedure LoadMenu(aMenuName : String;aHint : String = '';aShow : Boolean = True);
     procedure InitMenu;
     procedure ChangeConnect;
   public
@@ -125,10 +123,6 @@ uses
   FileCtrl, StrUtils, unitStandardHandle, formTableProperty, unitExcelHandle,
   formExport, formAbout, formImport, unitConfig, unitHistory, formSavePath,
   formSet,formSelectAll,frmMain;
-
-const
-  VERSION_URL = 'http://sz-btfs.yun.ftn.qq.com/ftn_handler/2b9f99e1565690cd424cf4a2cc2ff3ad4da93155f7ea9365baf46d09c876a3d1/?fname=Version.ini&from=30111&version=2.0.0.2&uin=190200649';
-  UPGRADE_URL = 'http://sz-btfs.yun.ftn.qq.com/ftn_handler/3b301df756b88a0b720798d077c605b69eeda0113c9dc0cc8624625e70dd80d2/?fname=Upgrade.zip&from=30111&version=2.0.0.2&uin=190200649';
 
 
 {$R *.dfm}
@@ -294,89 +288,13 @@ begin
 end;
 
 
-procedure TfmMain.MenuClick(Sender: TObject);
-var
-  aClassName : string;
-  aMenuName : string;
-  aNotShowFormHint : string;
-  aShow : Boolean;
-begin
-  Config.SystemParameter := FParameter;
-  aMenuName := (Sender as TMenuItem).Name;
-  aNotShowFormHint := (Sender as TMenuItem).Hint;
-  aClassName := Copy(aMenuName,5,Length(aMenuName)-4);
-  aShow := (LeftStr(aMenuName,4)  <> 'Unit');
-  LoadMenu(aClassName,aNotShowFormHint,aShow);
-end;
-
-
-
 
 procedure TfmMain.InitMenu;
 var
-  MenuItem,MenuSubItem:TMenuItem;
-  I ,J: Integer;
-  MenuNum : Integer;  
+  aLoadMenu : TLoadMenu;
 begin
-  MenuNum := 0;
-  for I := 0 to Length(Config.FMenuList) - 1 do
-  begin
-    if Config.FMenuList[I].ParentName <> '' then
-      Continue;
-      
-    MenuItem:=TMenuItem.Create(MainMenu);
-    if Config.FMenuList[I].ClassName = '' then
-    begin
-      MenuItem.Name := 'Unit' + IntToStr(Random(100));
-    end
-    else
-    begin
-      if Config.FMenuList[I].ClassType = 'Class' then
-      begin
-        MenuItem.Name := 'Unit' + Config.FMenuList[I].ClassName;
-      end
-      else
-      begin
-        MenuItem.Name := 'Form' + Config.FMenuList[I].ClassName;
-      end;    
-    end;
-    MenuItem.Caption:= Config.FMenuList[I].Caption;
-    MenuItem.Hint := Config.FMenuList[I].NotShowFormHint;
-    MenuItem.Visible := Config.FMenuList[I].Visible;
-    if Config.FMenuList[I].ClassName <> '' then
-      MenuItem.OnClick := MenuClick;
-    MainMenu.Items.Add(MenuItem);
-    for J := 0 to Length(Config.FMenuList) - 1 do
-    begin
-      if (Config.FMenuList[I].Name = Config.FMenuList[J].ParentName)   then
-      begin
-        MenuSubItem:=TMenuItem.Create(MainMenu);
-        if Config.FMenuList[J].ClassName = '' then
-        begin
-          MenuSubItem.Name := 'Unit' + IntToStr(Random(100));
-        end
-        else
-        begin
-          if Config.FMenuList[J].ClassType = 'Class' then
-          begin
-            MenuSubItem.Name := 'Unit' + Config.FMenuList[J].ClassName;
-          end
-          else
-          begin
-            MenuSubItem.Name := 'Form' + Config.FMenuList[J].ClassName;
-          end;
-        end;
-        MenuSubItem.Caption:= Config.FMenuList[J].Caption;
-        MenuSubItem.Hint := Config.FMenuList[J].NotShowFormHint;
-        MenuSubItem.Visible := Config.FMenuList[J].Visible;
-        if Config.FMenuList[J].ClassName <> '' then
-          MenuSubItem.OnClick := MenuClick;
-
-        MainMenu.Items[MenuNum].Add(MenuSubItem);
-      end;
-    end;    
-    Inc(MenuNum);
-  end;
+  aLoadMenu := TLoadMenu.Create;
+  aLoadMenu.Load(MainMenu,FParameter,PageSelect.ActivePageIndex,edtTable.Text);
 end;
 
 procedure TfmMain.LoadConfig;
@@ -646,38 +564,6 @@ begin
   FResult.DeleteRow;
   WorkRun;
 end;
-
-procedure TfmMain.LoadMenu(aMenuName : String;aHint : String = '';aShow : Boolean = True);
-var
-  aMenu : TfmParentMenu;
-begin
-  TfmMain.CreateInstance(aMenu,aMenuName);
-  try
-    if not aMenu.CheckIsShow then
-    begin
-      if aHint <> '' then
-      begin
-        ShowMessage(aHint);
-      end
-      else
-      begin
-        ShowMessage('无法使用该功能。');
-      end;
-      Exit;
-    end;
-    if aShow then
-    begin
-      aMenu.ShowModal;    
-    end
-    else
-    begin
-      aMenu.MenuHandle(FParameter,PageSelect.ActivePageIndex,edtTable.Text);
-    end;
-  finally
-    aMenu.Free;
-  end;
-end;
-
 
 procedure TfmMain.ChangeConnect;
 begin
